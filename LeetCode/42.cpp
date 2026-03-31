4e40ef2e29ef6d179885f583422e5fc97cc2b28e
lonespire <2206794306@qq.com>
2026-03-31 16:14:38 +0800

    feat: 实现单调栈方法解决接雨水问题
    使用单调递减队列来跟踪可能形成容器的边界，通过动态计算每个位置能接的雨水量

--- LeetCode/42.cpp
+++ LeetCode/42.cpp
@@ -1,16 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-class Solution {
+class Solution
+{
 public:
-    int trap(vector<int>& height) {
-        
+    int trap(vector<int> &height) // 采取单调递减队列，我理解为不断更新并储存上一个最大边
+    {
+        deque<int> q;
+        int ans = 0, n = height.size();
+        for (int i = 0; i < n; i++)
+        {
+            int min_height = 0;
+            while (!q.empty() && height[q.back()] < height[i])
+            {
+                min_height = max(min_height, height[q.back()]);
+                q.pop_back();
+                if (!q.empty())
+                    ans += (min(height[q.back()], height[i]) - min_height) * (i - q.back() - 1);
+            }
+            q.push_back(i);
+        }
+        return ans;
     }
 };
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
-    
+    Solution t;
+    vector<int> test = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    cout << t.trap(test);
     return 0;
 }
